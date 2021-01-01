e3bfb431489a444f786e60c769562fe6b740d660
Damini Sherathiya <daminisherathiya@gmail.com>
2021-01-01 19:57:26 +0530

    Fixes.

--- js/community.js
+++ js/community.js
@@ -1,5 +1,10 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+ return new bootstrap.Tooltip(tooltipTriggerEl)
+});
 
-  video_btn();
+
+video_btn();
 
 
 function video_btn() {
