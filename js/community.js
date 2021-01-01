b8eb7ce499958c4a9f6264a09d45d8175ebae22a
Damini Sherathiya <daminisherathiya@gmail.com>
2021-01-01 19:30:24 +0530

    Fixes.

---
+++ js/community.js
@@ -0,0 +1,13 @@
+
+  video_btn();
+
+
+function video_btn() {
+  document.querySelector(".btn-setter").addEventListener("click",function(){
+    document.querySelector(".video-container .btn-setter").remove();
+    document.querySelector(".video-container img").remove();
+    document.querySelector(".embed-responsive").classList.remove("d-none");
+    document.querySelector("iframe").setAttribute("src", document.querySelector("iframe").getAttribute("src") + "?autoplay=1");
+    // $("iframe")[0].src += "?autoplay=1";
+  })
+}
